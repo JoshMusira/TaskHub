ee1915b299c8f9c84857b1c59b120b145bc5827b
JoshMusira <joshuamusira01@gmail.com>
2023-08-06 00:28:12 +0300

    feat: create a todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 import InputField from './components/inputField/InputField'
+import { Todo } from './model/model'
 
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>("")
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo("");
+    }
+  }
+  console.log(todos);
 
   return (
     <div className='App'>
       <span className="heading">TaskHub</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {
+        todos.map((t) => (
+          <li>{t.todo}</li>
+        ))
+      }
     </div >
   )
 }
--- src/components/inputField/InputField.tsx
+++ src/components/inputField/InputField.tsx
@@ -1,9 +1,22 @@
+import { useRef } from 'react';
 import './inputfield.css'
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
     return (
-        <form className='input'>
-            <input type="text" className='input__box ' placeholder='Enter a task...' />
+        <form className='input' onSubmit={(e) => {
+            handleAdd(e)
+            inputRef.current?.blur();
+        }}>
+            <input type="text" className='input__box '
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+                placeholder='Enter a task...' />
             <button type="submit" className="input_submit">Go</button>
         </form>
 
